9776b5f8906d39b310c170ba4f5c3e24e4786a05
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-01-17 17:13:52 -0800

    python now handles passing rpc data

--- worker.py
+++ worker.py
@@ -2,6 +2,9 @@
 import time
 import os
 import pika
+import json
+import atexit
+
 print "worker"
 
 
@@ -27,10 +30,29 @@ writechannel = connection.channel()
 writechannel.queue_declare('db_write', durable=True)
 
 def callback(ch, method, properties, body):
+    # the key part of this code here is that the python worker can do anything it needs within this timelimit and could also start additional long tasks
+    # in this scope, we have access to the user id, the question id, and the users choice
+    # but it makes sure that it passes the correlation_id(if any) to the db worker, as well as the reply_to
+    # this insures that the data can be returned on the original channel and the api can respond.
+
     print " [x] Received %r" % body
     print " [x] Done"
-    writechannel.basic_publish(exchange='', routing_key='db_queue', body=str(body), properties=pika.BasicProperties(
-        delivery_mode=2
+    j = json.loads(body)
+    print j['questionId']
+    newId = j['questionId'] + 1
+    rpc = {
+        'method': 'findQuestion',
+        'arguments': [newId]
+    }
+    message = json.dumps(rpc)
+    routing_key = 'db_rpc_worker'
+    correlation_id = properties.correlation_id
+    reply_to = properties.reply_to
+
+    writechannel.basic_publish(exchange='', routing_key=routing_key, body=message, properties=pika.BasicProperties(
+        delivery_mode=2,
+        correlation_id=correlation_id,
+        reply_to=reply_to
     ))
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
@@ -38,5 +60,10 @@ def callback(ch, method, properties, body):
 channel.basic_consume(callback,
                       queue='task_queue')
 
+def exit_handler():
+    channel.close();
 print ' [*] Waiting for messages. To exit press CTRL+C'
+
+atexit.register(exit_handler)
 channel.start_consuming()
+
